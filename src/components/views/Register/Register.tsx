2ee6e0f059a1c18807f2f5a9a173e0a228433482
Mahesaadikusuma <mahesaadikuzuma@gmail.com>
2025-10-27 19:03:16 +0700

    feat: register

--- src/components/views/Register/Register.tsx
+++ src/components/views/Register/Register.tsx
@@ -1,11 +1,23 @@
-import { Button, Card, CardBody, Input } from "@heroui/react";
+import { Button, Card, CardBody, Input, Spinner } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 import useRegister from "./useRegister";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Controller } from "react-hook-form";
+import { cn } from "@/utils/cn";
 
 const Register = () => {
-  const { visiblePassword, handleVisiblePassword } = useRegister();
+  const {
+    visiblePassword,
+    handleVisiblePassword,
+    control,
+    errors,
+    handleRegister,
+    handleSubmit,
+    isPendingRegister,
+  } = useRegister();
+
+  console.log(errors);
   return (
     <div className="flex w-full flex-col items-center justify-center lg:flex-row lg:gap-20">
       <div className="mb-10 flex w-full flex-col items-center justify-center gap-10 lg:mb-0 lg:w-1/3">
@@ -33,78 +45,124 @@ const Register = () => {
                Login here
             </Link>
           </p>
+          {errors.root && (
+            <p className="text-danger text-medium">{errors?.root?.message}</p>
+          )}
+          <form
+            onSubmit={handleSubmit(handleRegister)}
+            className={cn(
+              "flex w-full flex-col",
+              Object.keys(errors).length > 0 ? "gap-2" : "gap-4",
+            )}
+          >
+            <Controller
+              name="fullName"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  label="Full Name"
+                  type="text"
+                  variant="bordered"
+                  autoComplete="off"
+                  isInvalid={errors.fullName !== undefined}
+                  errorMessage={errors.fullName?.message}
+                />
+              )}
+            />
 
-          <form className="flex w-full flex-col gap-4">
-            <Input
-              label="FullName"
-              type="text"
-              variant="bordered"
-              autoComplete="off"
+            <Controller
+              name="username"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="text"
+                  label="Username"
+                  variant="bordered"
+                  autoComplete="off"
+                  isInvalid={errors.username !== undefined}
+                  errorMessage={errors.username?.message}
+                />
+              )}
             />
-            <Input
-              label="UserName"
-              type="text"
-              variant="bordered"
-              autoComplete="off"
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="email"
+                  label="Email"
+                  variant="bordered"
+                  autoComplete="off"
+                  isInvalid={errors.email !== undefined}
+                  errorMessage={errors.email?.message}
+                />
+              )}
             />
-            <Input
-              label="Email"
-              type="email"
-              variant="bordered"
-              autoComplete="off"
+            <Controller
+              name="password"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type={visiblePassword.password ? "text" : "password"}
+                  label="Password"
+                  variant="bordered"
+                  autoComplete="off"
+                  isInvalid={errors.password !== undefined}
+                  errorMessage={errors.password?.message}
+                  endContent={
+                    <button
+                      className="focus:outline-none"
+                      type="button"
+                      onClick={() => handleVisiblePassword("password")}
+                    >
+                      {visiblePassword.password ? (
+                        <FaEye className="text-default-400 pointer-events-none text-xl" />
+                      ) : (
+                        <FaEyeSlash className="text-default-400 pointer-events-none text-xl" />
+                      )}
+                    </button>
+                  }
+                />
+              )}
             />
-            <Input
-              label="Password"
-              type={visiblePassword.password ? "text" : "password"}
-              variant="bordered"
-              autoComplete="off"
-              endContent={
-                <button
-                  className="focus:outline-none"
-                  type="button"
-                  onClick={() => handleVisiblePassword("password")}
-                >
-                  {visiblePassword.password ? (
-                    <FaEye
-                      size={20}
-                      className="text-default-400 pointer-events-none text-xl"
-                    />
-                  ) : (
-                    <FaEyeSlash
-                      size={20}
-                      className="text-default-400 pointer-events-none text-xl"
-                    />
-                  )}
-                </button>
-              }
-            />
-            <Input
-              label="Password Confirmation"
-              type={visiblePassword.passwordConfirmation ? "text" : "password"}
-              variant="bordered"
-              autoComplete="off"
-              endContent={
-                <button
-                  className="focus:outline-none"
-                  type="button"
-                  onClick={() => handleVisiblePassword("passwordConfirmation")}
-                >
-                  {visiblePassword.passwordConfirmation ? (
-                    <FaEye
-                      size={20}
-                      className="text-default-400 pointer-events-none text-xl"
-                    />
-                  ) : (
-                    <FaEyeSlash
-                      size={20}
-                      className="text-default-400 pointer-events-none text-xl"
-                    />
-                  )}
-                </button>
-              }
+            <Controller
+              name="confirmPassword"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type={visiblePassword.confirmPassword ? "text" : "password"}
+                  label="Password Confirmation"
+                  variant="bordered"
+                  autoComplete="off"
+                  isInvalid={errors.confirmPassword !== undefined}
+                  errorMessage={errors.confirmPassword?.message}
+                  endContent={
+                    <button
+                      className="focus:outline-none"
+                      type="button"
+                      onClick={() => handleVisiblePassword("confirmPassword")}
+                    >
+                      {visiblePassword.confirmPassword ? (
+                        <FaEye className="text-default-400 pointer-events-none text-xl" />
+                      ) : (
+                        <FaEyeSlash className="text-default-400 pointer-events-none text-xl" />
+                      )}
+                    </button>
+                  }
+                />
+              )}
             />
             <Button color="danger" size="lg" type="submit">
-              Register
+              {isPendingRegister ? (
+                <Spinner color="white" size="sm" />
+              ) : (
+                "Register"
+              )}
             </Button>
           </form>
         </CardBody>
--- src/components/views/Register/useRegister.ts
+++ src/components/views/Register/useRegister.ts
@@ -1,21 +1,80 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { IRegister } from "@/types/Auth";
+import authServices from "@/services/auth";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
+const registerSchema = yup.object().shape({
+  fullName: yup.string().required("Please Input Full Name"),
+  username: yup.string().required("Please Input username"),
+  email: yup.string().email().required("Please Input email"),
+  password: yup
+    .string()
+    .min(8, "minimal 8 characters")
+    .required("Please Input password"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), ""], "Passwords must match")
+    .required("Please Input password confirmation"),
+});
 
 const useRegister = () => {
+  const router = useRouter();
   const [visiblePassword, setVisiblePassword] = useState({
     password: false,
-    passwordConfirmation: false,
+    confirmPassword: false,
   });
 
-  const handleVisiblePassword = (key: "password" | "passwordConfirmation") => {
+  const handleVisiblePassword = (key: "password" | "confirmPassword") => {
     setVisiblePassword(() => ({
       ...visiblePassword,
       [key]: !visiblePassword[key],
     }));
   };
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setError,
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const registerService = async (payload: IRegister) => {
+    const result = await authServices.register(payload);
+    return result;
+  };
+
+  const { mutate: mutateRegister, isPending: isPendingRegister } = useMutation({
+    mutationFn: registerService,
+    onError: (error) => {
+      setError("root", {
+        message: error.message,
+      });
+    },
+    onSuccess: (data) => {
+      router.push("/auth/register/success");
+      reset();
+    },
+  });
+
+  const handleRegister = (data: IRegister) => {
+    mutateRegister(data);
+  };
+
   return {
     visiblePassword,
     handleVisiblePassword,
+    control,
+    handleSubmit,
+    handleRegister,
+    isPendingRegister,
+    errors,
   };
 };
 
